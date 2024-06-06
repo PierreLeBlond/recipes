4279cb58e142c5ec44da54f7a401017f39e6ccaa
PierreLeBlond <pierre.lespingal@gmail.com>
2024-06-06 18:04:26 +0200

    feat: Add button shadows and borders

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/src/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-xs shadow-xs hover:shadow-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-xs shadow-lg hover:shadow-md transition-shadow font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -17,8 +17,8 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-link border border-link shadow-link",
-        edit: "bg-edit text-edit-foreground",
+        link: "text-link border border-link shadow-link/50",
+        edit: "bg-edit border border-edit text-edit-foreground shadow-edit/50",
       },
       size: {
         default: "h-10 px-4 py-2",
