3ce907c03bfdca894bc120da0f61cbfbdd778a17
PierreLeBlond <pierre.lespingal@gmail.com>
2024-06-30 16:37:28 +0200

    feat: Add getCaretCoordinates

---
+++ src/app/components/recipe/steps/step/description/ContentEditable/getCaretCoordinates.ts
@@ -0,0 +1,31 @@
+export const getCaretCoordinates = (element: HTMLElement) => {
+  if (!window.getSelection) {
+    // Does not support IE8 and below
+    return null;
+  }
+
+  if (document.activeElement !== element) {
+    return null;
+  }
+
+  const selection = window.getSelection();
+
+  if (!selection?.rangeCount) {
+    return null;
+  }
+
+  const range = selection.getRangeAt(0);
+  const preCaretRange = range.cloneRange();
+  preCaretRange.collapse(true);
+
+  const rect = preCaretRange.getClientRects()[0];
+
+  if (!rect) {
+    return null;
+  }
+
+  return {
+    x: rect.left | element.getBoundingClientRect().left,
+    y: rect.top | element.getBoundingClientRect().top
+  }
+};
---
+++ tests/app/components/recipe/steps/step/description/contentEditable/getCaretCoordinates.test.ts
@@ -0,0 +1,48 @@
+import userEvent from "@testing-library/user-event";
+import { test, expect, beforeEach, vi } from "vitest";
+import { getCaretCoordinates } from "@/src/app/components/recipe/steps/step/description/ContentEditable/getCaretCoordinates";
+
+Range.prototype.getClientRects = () => {
+    return {
+      item: () => null,
+      length: 1,
+      [0]: {
+        left: 0,
+        top: 0,
+      }
+    };
+  };
+
+const getContentEditableElement = () => {
+  const element = document.querySelector("[contenteditable]");
+  if (!element) {
+    throw new Error("Element not found");
+  }
+  return element as HTMLElement;
+};
+
+const user = userEvent.setup();
+
+beforeEach(() => {
+  const element = document.createElement("div");
+  element.setAttribute("contenteditable", "true");
+  document.body.appendChild(element);
+});
+
+test("Should exists", () => {
+  expect(getCaretCoordinates).toBeDefined();
+});
+
+test("Should return null if element is not focused", () => {
+  const element = getContentEditableElement();
+
+  expect(getCaretCoordinates(element)).toBeNull();
+});
+
+test("Should return caret position within its container", async () => {
+  const element = getContentEditableElement();
+
+  await user.click(element);
+
+  expect(getCaretCoordinates(element)).toEqual({x:0, y:0});
+});
