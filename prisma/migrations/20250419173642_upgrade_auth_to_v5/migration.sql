780eb2b18ca97fcfdaee2e14a1c2c29dc9a51aab
PierreLeBlond <pierre.lespingal@gmail.com>
2025-04-19 19:37:48 +0200

    fix: Fix db migration

--- prisma/migrations/20250419173642_upgrade_auth_to_v5/migration.sql
+++ prisma/migrations/20250419173642_upgrade_auth_to_v5/migration.sql
@@ -7,9 +7,6 @@
   - You are about to drop the column `refresh_token_expires_in` on the `Account` table. All the data in the column will be lost.
   - The primary key for the `Session` table will be changed. If it partially fails, the table could be left without primary key constraint.
   - You are about to drop the column `id` on the `Session` table. All the data in the column will be lost.
-  - Added the required column `updatedAt` to the `Account` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `updatedAt` to the `Session` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `updatedat` to the `User` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
@@ -17,18 +14,18 @@ ALTER TABLE "Account" DROP CONSTRAINT "Account_pkey",
 DROP COLUMN "expires_at",
 DROP COLUMN "id",
 DROP COLUMN "refresh_token_expires_in",
-ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+ADD COLUMN     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3);
 
 -- AlterTable
 ALTER TABLE "Session" DROP CONSTRAINT "Session_pkey",
 DROP COLUMN "id",
-ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+ADD COLUMN     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3);
 
 -- AlterTable
-ALTER TABLE "User" ADD COLUMN     "createdat" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "updatedat" TIMESTAMP(3) NOT NULL;
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3);
 
 -- AlterTable
 ALTER TABLE "VerificationToken" ADD CONSTRAINT "VerificationToken_pkey" PRIMARY KEY ("identifier", "token");
